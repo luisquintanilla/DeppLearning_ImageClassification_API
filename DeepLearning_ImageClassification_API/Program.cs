97f0366e6f506cfcf575dc05b7ab13ad8fcab87a
Luis Quintanilla <luquinta@microsoft.com>
2019-10-08 12:40:30 -0400

    Image Classification API Sample

---
+++ DeepLearning_ImageClassification_API/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Transforms;
+
+namespace DeepLearning_ImageClassification_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assetsRelativePath = "../../../assets";
+
+            MLContext mlContext = new MLContext();
+
+            IEnumerable<ModelInput> images = LoadImagesFromDirectory(folder: assetsRelativePath, useFolderNameAsLabel: true);
+
+            IDataView imageData = mlContext.Data.LoadFromEnumerable(images);
+
+            IDataView shuffledData = mlContext.Data.ShuffleRows(imageData);
+
+            TrainTestData dataSplit = mlContext.Data.TrainTestSplit(shuffledData);
+            IDataView trainSet = dataSplit.TrainSet;
+            IDataView validationSet = dataSplit.TestSet;
+
+            var mapLabelEstimator = mlContext.Transforms.Conversion.MapValueToKey
+                (outputColumnName: "LabelAsKey",
+                    inputColumnName: "Label",
+                    keyOrdinality: ValueToKeyMappingEstimator.KeyOrdinality.ByValue);
+
+            IDataView transformedValidationData = mapLabelEstimator
+                                        .Fit(validationSet)
+                                        .Transform(validationSet);
+
+            var trainingPipeline =
+                mapLabelEstimator
+                .Append(mlContext.Model.ImageClassification(
+                    featuresColumnName: "ImagePath",
+                    labelColumnName: "LabelAsKey",
+                    arch: ImageClassificationEstimator.Architecture.ResnetV2101,
+                    epoch: 100,
+                    batchSize: 20,
+                    testOnTrainSet: false,
+                    metricsCallback: (metrics) => Console.WriteLine(metrics),
+                    validationSet: transformedValidationData,
+                    reuseTrainSetBottleneckCachedValues: true,
+                    reuseValidationSetBottleneckCachedValues: true));
+
+            ITransformer trainedModel = trainingPipeline.Fit(trainSet);
+
+            ClassifyImages(mlContext, validationSet, trainedModel);
+
+            Console.ReadKey();
+        }
+
+        public static void ClassifyImages(MLContext mlContext, IDataView data, ITransformer trainedModel)
+        {
+            IDataView predictionData = trainedModel.Transform(data);
+
+            var keyMappings = GetKeyMappings(predictionData);
+
+            IEnumerable<ModelOutput> predictions = mlContext.Data.CreateEnumerable<ModelOutput>(predictionData, reuseRowObject: true).Take(10);
+
+            foreach (var prediction in predictions)
+            {
+                var predictedLabelValue = keyMappings[prediction.PredictedLabel];
+                Console.WriteLine($"Actual Value: {prediction.Label} | Predicted Value: {predictedLabelValue}");
+            }
+        }
+
+        public static IEnumerable<ModelInput> LoadImagesFromDirectory(string folder, bool useFolderNameAsLabel = true)
+        {
+            var files = Directory.GetFiles(folder, "*",
+                searchOption: SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                if ((Path.GetExtension(file) != ".jpg") && (Path.GetExtension(file) != ".png"))
+                    continue;
+
+                var label = Path.GetFileName(file);
+                if (useFolderNameAsLabel)
+                    label = Directory.GetParent(file).Name;
+                else
+                {
+                    for (int index = 0; index < label.Length; index++)
+                    {
+                        if (!char.IsLetter(label[index]))
+                        {
+                            label = label.Substring(0, index);
+                            break;
+                        }
+                    }
+                }
+
+                yield return new ModelInput()
+                {
+                    ImagePath = file,
+                    Label = label
+                };
+            }
+        }
+
+        private static ReadOnlyMemory<char>[] GetKeyMappings(IDataView predictions)
+        {
+            VBuffer<ReadOnlyMemory<char>> keys = default;
+            predictions.Schema["LabelAsKey"].GetKeyValues(ref keys);
+            var originalValues = keys.DenseValues().ToArray();
+            return originalValues;
+        }
+    }
+    
+    class ModelInput
+    {
+        public string ImagePath { get; set; }
+
+        public string Label { get; set; }
+    }
+
+    class ModelOutput
+    {
+        public string Label { get; set; }
+
+        public UInt32 PredictedLabel { get; set; }
+    }
+}
